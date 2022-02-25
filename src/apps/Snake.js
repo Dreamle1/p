fce2606228a9843abf13a22dd923aab227a7c62c
Dreamle1 <35672312+Dreamle1@users.noreply.github.com>
2022-02-25 15:53:17 -0800

    fruit

--- src/apps/Snake.js
+++ src/apps/Snake.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
+const gridsize = { x: 20, y: 20 };
+
 const useStyles = makeStyles(() => ({
   block: {
     backgroundColor: "#000",
@@ -22,7 +24,8 @@ function Snake() {
       .fill(0)
       .map((row) => new Array(20).fill(0))
   );
-  const [timer, settimer] = useState(100);
+  const [timer, settimer] = useState(200);
+  const [fruit, setfruit] = useState({ x: 0, y: 0 });
   const [snake, setsnake] = useState({
     head: { x: 10, y: 10 },
     pos: [
@@ -31,84 +34,127 @@ function Snake() {
     ],
     dir: 0,
   });
+  const [keydir, setkeydir] = useState(0);
 
   useEffect(() => {
+    resetfruit();
     const interval = setInterval(() => {
       update();
     }, timer);
     document.addEventListener("keydown", keydown, false);
-    return () => clearInterval(interval);
+    return () => {
+      clearInterval(interval);
+      document.removeEventListener("keydown", keydown, false);
+    };
   }, []);
 
+  const resetfruit = () => {
+    setfruit((fruit) => {
+      fruit.x = Math.floor(Math.random() * (gridsize.x - 1));
+      fruit.y = Math.floor(Math.random() * (gridsize.y - 1));
+      return fruit;
+    });
+  };
+
   const keydown = (e) => {
-    const snakeobj = snake;
-    if (e.keyCode == 39 && snakeobj.dir != 3) {
-      snakeobj.dir = 1;
-    } else if (e.keyCode == 40 && snakeobj.dir != 2) {
-      snakeobj.dir = 0;
-    } else if (e.keyCode == 37 && snakeobj.dir != 1) {
-      snakeobj.dir = 3;
-    } else if (e.keyCode == 38 && snakeobj.dir != 0) {
-      snakeobj.dir = 2;
+    if (e.keyCode === 39) {
+      setkeydir(1); //right
+    } else if (e.keyCode === 40) {
+      setkeydir(0); //down
+    } else if (e.keyCode === 37) {
+      setkeydir(3); //left
+    } else if (e.keyCode === 38) {
+      setkeydir(2); //up
     }
   };
 
   const update = () => {
     move();
-    updateGrid();
   };
 
-  const updateGrid = () => {
-    let gridobj = Array(20)
-      .fill(0)
-      .map((row) => new Array(20).fill(0));
+  useEffect(() => {
+    setsnake((s) => {
+      if (
+        (s.dir !== 3 && keydir === 1) ||
+        (s.dir !== 2 && keydir === 0) ||
+        (s.dir !== 1 && keydir === 3) ||
+        (s.dir !== 0 && keydir === 2)
+      )
+        return { ...s, dir: keydir };
+      return s;
+    });
+  }, [keydir]);
 
-    for (const coord of snake.pos) {
-      gridobj[coord.x][coord.y] = 1;
-    }
-    setgrid(gridobj);
-  };
-
-  const move = () => {
-    let snakeobj = snake;
-    let newpos = {};
-    switch (snake.dir) {
-      case 0:
-        newpos = { x: snake.head.x + 1, y: snake.head.y };
-        break;
-      case 1:
-        newpos = { x: snake.head.x, y: snake.head.y + 1 };
-        break;
-      case 2:
-        newpos = { x: snake.head.x - 1, y: snake.head.y };
-        break;
-      default:
-        newpos = { x: snake.head.x, y: snake.head.y - 1 };
-    }
-    snakeobj.pos.push(newpos);
-    snakeobj.pos.shift();
-    snakeobj.head = newpos;
+  useEffect(() => {
     if (
-      snake.head.x > 20 ||
+      snake.head.x > gridsize.x - 1 ||
       snake.head.x < 0 ||
-      snake.head.y > 20 ||
+      snake.head.y > gridsize.y - 1 ||
       snake.head.y < 0
     ) {
       resetgame();
-    } else {
-      setsnake(snakeobj);
+      return;
     }
+    if (snake.head.x === fruit.x && snake.head.y === fruit.y) {
+      console.log("yummy");
+      resetfruit();
+    }
+    updateGrid();
+  }, [snake]);
+
+  const updateGrid = () => {
+    setgrid((grid) => {
+      grid = Array(20)
+        .fill(0)
+        .map((row) => new Array(20).fill(0));
+      grid[fruit.x][fruit.y] = 2;
+      for (const coord of snake.pos) {
+        grid[coord.x][coord.y] = 1;
+      }
+      let newgrid = [...grid];
+      return newgrid;
+    });
+  };
+
+  const move = () => {
+    setsnake((prevsnake) => {
+      let newpos = {};
+      let newsnake = Object.assign({}, prevsnake);
+      newsnake.pos = [...prevsnake.pos];
+      switch (newsnake.dir) {
+        case 0:
+          newpos = { x: newsnake.head.x + 1, y: newsnake.head.y };
+          break;
+        case 1:
+          newpos = { x: newsnake.head.x, y: newsnake.head.y + 1 };
+          break;
+        case 2:
+          newpos = { x: newsnake.head.x - 1, y: newsnake.head.y };
+          break;
+        default:
+          newpos = { x: newsnake.head.x, y: newsnake.head.y - 1 };
+      }
+      newsnake.pos.push(newpos);
+      if (newpos.x === fruit.x && newpos.y === fruit.y) {
+      } else {
+        newsnake.pos.shift();
+      }
+      newsnake.head = newpos;
+      return newsnake;
+    });
   };
 
   const resetgame = () => {
-    let snakeobj = snake;
-    snakeobj.head = { x: 10, y: 10 };
-    snakeobj.pos = [
-      { x: 9, y: 10 },
-      { x: 10, y: 10 },
-    ];
-    snakeobj.dir = 0;
-    setsnake(snakeobj);
+    setsnake((snake) => {
+      snake.head = { x: 10, y: 10 };
+      snake.pos = [
+        { x: 9, y: 10 },
+        { x: 10, y: 10 },
+      ];
+      snake.dir = 0;
+      return snake;
+    });
+    resetfruit();
   };
 
   const getColor = (val) => {
@@ -138,7 +184,6 @@ function Snake() {
       </div>
     );
   };
-
   return <div>{creategrid(grid)}</div>;
 }
 
